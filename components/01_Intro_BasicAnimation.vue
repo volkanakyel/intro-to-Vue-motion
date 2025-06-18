6d764551576303a2c9cfdc0a092d6f46dd4035a8
Volkan Akyel <volkan@uikielle.com>
2025-06-18 18:44:56 +0800

    add 01 component for basic animation

---
+++ components/01_Intro_BasicAnimation.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <Motion
+    :initial="{ opacity: 0, x: -50 }"
+    :animate="{ opacity: 1, x: 0 }"
+    :transition="{ type: 'spring', stiffness: 100, damping: 10 }"
+    class="text-2xl font-bold text-center p-8 bg-white rounded-xl shadow-sm"
+  >
+    Hello Vue Motion!
+  </Motion>
+</template>
